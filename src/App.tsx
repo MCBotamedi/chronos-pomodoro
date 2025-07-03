bbbd05eb51043d9039aaf99befc97b40edbf2e8c
MCBotamedi <botamedis@gmail.com>
2025-07-03 20:40:24 -0300

    atualização04  03-07-2025

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   console.log('Oi');
 
   return (
-    <div>
-      <Heading attr={123} attr2='String'>
+    <>
+      <Heading>
         Olá Mundo!
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
-      <Heading>Olá Mundo!2</Heading>
-      <Heading>Cronos</Heading>
+
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi,
         iste laudantium. Aliquid commodi dicta fugiat doloremque ipsum
         consequatur esse obcaecati cupiditate beatae quibusdam natus, harum
         libero, ab temporibus, fuga hic!
       </p>
-    </div>
+    </>
   );
 }
