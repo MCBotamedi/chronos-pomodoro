7fd0a9ae81468ce99d31861b381259f658c19080
MCBotamedi <botamedis@gmail.com>
2025-07-04 14:48:53 -0300

    atualização03 04-07-2025

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container } from './components/Container';
 import { Menu } from './components/Menu';
 import { Logo } from './components/Logo';
+import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -11,8 +12,32 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 </p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
