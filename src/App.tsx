04a6bd09494685a9c31f6cea27776a4aeb114a78
MCBotamedi <botamedis@gmail.com>
2025-07-04 21:46:03 -0300

    atualização11 04-07-2025

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,11 @@ import './styles/theme.css';
 import './styles/global.css';
 import { PlayCircleIcon } from 'lucide-react';
 
-import { Heading } from './components/Heading';
-import { useState } from 'react';
+
 
 export function App() {
-  // Que todos os componentes que usam "numero"
-  //saibam das mudanças em seu valor.
-  //Sempre que eu usar useState, não vou usar atribuição diretamente.
-  //const [numero, setNumero] = useState(() => {
-  // console.log('Lazy initialization');
-  // return 0;
-  //});
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    // setNumero(prevState => prevState + 1); // Atualiza o estado com base no valor anterior
-    setNumero(1);
-  }
-
   return (
     <>
-      <Heading>Numero: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
-
       <Container>
         <Logo />
       </Container>
@@ -50,7 +32,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText={numero.toString()}
+              labelText='task'
               id='meuInput'
               type='text'
               placeholder='Digite algo...'
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,54 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
 
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-  return (<nav className={styles.menu}>
-    <a className={styles.menuLink} href="#">
-      <HouseIcon />
-    </a>
-    <a className={styles.menuLink} href="#">
-      <HistoryIcon />
-    </a>
-    <a className={styles.menuLink} href="#">
-      <SettingsIcon />
-    </a>
-    <a className={styles.menuLink} href="#">
-      <SunIcon />
-    </a>
-  </nav>
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault(); //não segue o link
+    console.log('Clicado', Date.now());
+  }
+
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para Home'
+        title='Ir para Home'
+      >
+        <HouseIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver Histórico'
+        title='ver Histórico'
+      >
+        <HistoryIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para Configurações'
+        title='Ir para Configurações'
+      >
+        <SettingsIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar Tema'
+        title='Mudar Tema'
+        onClick={handleThemeChange}
+      >
+        <SunIcon />
+      </a>
+    </nav>
   );
 }
