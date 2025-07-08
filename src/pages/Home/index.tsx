c55a6692d167b057c47c91b50d9b7e56d2526ef5
MCBotamedi <botamedis@gmail.com>
2025-07-07 21:26:51 -0300

    atualização05 07-07-2025

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,27 @@
-import { Container } from "../../components/Container";
-import { CountDown } from "../../components/CountDown";
-import { MainForm } from "../../components/MainForm";
-import { MainTemplate } from "../../templates/MainTemplates";
+import type React from 'react';
+import { Container } from '../../components/Container';
+import { CountDown } from '../../components/CountDown';
+import { MainForm } from '../../components/MainForm';
+import type { TaskStateModel } from '../../models/TaskStateModel';
+import { MainTemplate } from '../../templates/MainTemplates';
 
-export function Home() {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomeProps) {
+  const { state, setState } = props;
+
   return (
     <MainTemplate>
-        <Container>
-          <CountDown />
-        </Container>
-        
-        <Container>
-          <MainForm />
-        </Container>
-        
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <MainForm />
+      </Container>
     </MainTemplate>
   );
 }
