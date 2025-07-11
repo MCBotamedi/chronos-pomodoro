5ac21c98c80151c063945c630fe07109f0ad7c9f
MCBotamedi <botamedis@gmail.com>
2025-07-11 16:23:18 -0300

    atualização03 11-07-2025

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TakdContext';
 import { taskReducer } from './TaskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
 import { TaskActionTypes } from './TaskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -11,6 +12,7 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   const worker = TimerWorkerManager.getInstance();
 
@@ -19,6 +21,12 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     //console.log(countDownSeconds);
 
     if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        console.log('Tocando áudio');
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
+      //)
       dispatch({
         type: TaskActionTypes.COMPLETE_TASK,
       });
@@ -39,6 +47,16 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     }
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      console.log('Cargando áudio');
+      playBeepRef.current = loadBeep();
+    } else {
+      console.log('Zerando áudio');
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
